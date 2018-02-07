2df29b6b34c24ee86995803494ade6f324ffeb38
willis plummer <willisplummer@gmail.com>
2018-02-07 08:32:27 -0500

    simplify

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const express = require("express");
-const router = express.Router();
-router.get("/", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
-});
-module.exports = router;
+const app = require("express")();
+const http = require("http").Server(app)
+const io = require("socket.io")(http);
+const port = process.env.PORT || 4001
+
+// dont need this for now
+
+// app.get('/', (req, res) => {
+//   res.send({ response: "I am alive" }).status(200);
+// })
+
+const initialData = []
+const room = 'theChat'
+
+io.on("connection", socket => {
+  console.log("New client connected")
+  socket.emit("update chat", initialData)
+  socket.join(room)
+
+  socket.on("data", (val) => {
+    initialData.push(val)
+    io.sockets.in(room).emit("update chat", initialData)
+  })
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected")
+    socket.leave(room)
+  })
+})
+
+http.listen(port, () =>
+  console.log(`Listening on port ${port}`)
+)
